1e643b618598ae905881f6bfaf11ff85bb116232
Kombu Takana <kombu-takana@users.noreply.github.com>
2022-02-01 16:38:45 -0500

    first commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, PullPayment, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+
+    /// @dev Base token URI used as a prefix by tokenURI().
+    string public baseTokenURI;
+
+    // Constants
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
+    
+    constructor() ERC721("PhantaBear Clone", "NFT") {}
+    
+    function mintTo(address recipient) public returns (uint256) {
+        uint256 currTokenId = currentTokenId.current();
+        require(currTokenId < TOTAL_SUPPLY, "Max mint supply reached!");
+        // require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+
+    /// @dev Returns a URI for a given token ID
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    /// @dev Sets the base token URI prefix.
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    /// @dev Overridden in order to make it an onlyOwner function
+    function withdrawPayments(address payable payee) public override onlyOwner virtual {
+        super.withdrawPayments(payee);
+    }
+}
